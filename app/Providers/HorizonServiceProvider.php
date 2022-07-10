7af3d07ce7fbe50080129c9604caca089d899279
twent <twentprod@gmail.com>
2022-07-10 11:36:05 +0300

    🛠️➕ Replace queue workers to Horizon

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+
+        // Horizon::night();
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        /* Allow Horizon Dashboard in production environment for Default User */
+        Gate::define('viewHorizon', function ($user) {
+            return in_array($user->email, [
+                'test@example.com'
+            ]);
+        });
+    }
+}
